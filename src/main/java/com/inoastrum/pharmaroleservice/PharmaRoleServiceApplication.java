ce739f9a046f31be3c4a1e66ccfb53809a442eac
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-08-09 11:26:38 -0700

    Init add

---
+++ src/main/java/com/inoastrum/pharmaroleservice/PharmaRoleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inoastrum.pharmaroleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaRoleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaRoleServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inoastrum/pharmaroleservice/PharmaRoleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inoastrum.pharmaroleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaRoleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
